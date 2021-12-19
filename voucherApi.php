fb04834f7feb050bc6122792b90e176239b9a697
bantolos <bantolo.setiadi@ti.ukdw.ac.id>
2021-12-19 18:55:43 +0700

    top up

--- voucherApi.php
+++ voucherApi.php
@@ -103,4 +103,182 @@ function get_voucher_by_id(){
     echo json_encode($response);
 }
 
+// Get Data Voucher By Kode Voucher
+function get_voucher(){
+    global $conn;
+    if ($_POST) {
+        $kode_voucher = $_POST["kode_voucher"];
+        $sql = "SELECT * FROM voucher WHERE kode_voucher = '$kode_voucher'";
+        $query = mysqli_query($conn, $sql);
+        $count = mysqli_num_rows($query);
+        $result = array();
+        if ($count > 0) {
+            while ($row = $query->fetch_assoc()) {
+                array_push($result, array(
+                    'id_voucher' => $row['id_voucher'],
+                    'id_user' => $row['id_user'],
+                    'kode_voucher' => $row['kode_voucher'],
+                    'status' => $row['status'],
+                    'nominal' => $row['nominal'],
+                    'tanggal' => $row['tanggal'],
+                ));
+            };
+        }
+        if ($result) {
+            $response = array(
+                'status' => 1,
+                'message' => 'Success',
+                'data' => $result
+            );
+        } else {
+            $response = array(
+                'status' => 0,
+                'message' => 'No data found'
+            );
+        }
+    } else {
+        $response = array(
+            'status' => -1,
+            'message' => 'Silakan input kode voucher'
+        );
+    }
+    header('Content-Type: application/json');
+    echo json_encode($response);
+}
+
+function cari_voucher($kode_voucher) 
+{
+    global $conn;
+    $result = mysqli_query($conn, "SELECT kode_voucher FROM voucher
+        WHERE kode_voucher = '$kode_voucher'");
+
+        if(mysqli_num_rows ($result) > 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+}
+
+function nominal_voucher($kode_voucher) 
+{
+    global $conn;
+    $result = mysqli_query($conn, "SELECT nominal FROM voucher
+        WHERE kode_voucher = '$kode_voucher'");
+        $row = mysqli_fetch_array($result);
+
+        if($result) {
+            return $row["nominal"];
+        }
+        else {
+            return false;
+        }
+}
+
+function status_voucher($kode_voucher) 
+{
+    global $conn;
+    $result = mysqli_query($conn, "SELECT status FROM voucher
+        WHERE kode_voucher = '$kode_voucher'");
+        $row = mysqli_fetch_array($result);
+
+        if($result) {
+            return $row["status"];
+        }
+        else {
+            return false;
+        }
+}
+
+function pemilik_voucher($kode_voucher) 
+{
+    global $conn;
+    $result = mysqli_query($conn, "SELECT id_user FROM voucher
+        WHERE kode_voucher = '$kode_voucher'");
+        $row = mysqli_fetch_array($result);
+
+        if($result) {
+            return $row["id_user"];
+        }
+        else {
+            return false;
+        }
+}
+
+function pakai_voucher($kode_voucher) 
+{
+    global $conn;
+    $result = mysqli_query($conn, "UPDATE voucher SET 
+        status = 'true'
+        WHERE kode_voucher = '$kode_voucher'");
+
+        if($result) {
+            return true;
+        }
+        else {
+            return false;
+        }
+}
+
+function card_uid($id_user) 
+{
+    global $conn;
+    $result = mysqli_query($conn, "SELECT card_uid FROM kartu
+        WHERE id_user = '$id_user'");
+        $row = mysqli_fetch_array($result);
+
+        if($result) {
+            return $row["card_uid"];
+        }
+        else {
+            return false;
+        }
+}
+
+function top_up(){
+    global $conn;
+    if ($_POST) {
+        $kode_voucher = $_POST["kode_voucher"];
+        if(cari_voucher($kode_voucher)) {
+            if(status_voucher($kode_voucher) == "false") {
+                $nominal = nominal_voucher($kode_voucher);
+                $id_user = pemilik_voucher($kode_voucher);
+                $card_uid = card_uid($id_user); 
+                $result = mysqli_query($conn, "UPDATE kartu SET 
+                saldo = saldo+$nominal
+                WHERE card_uid = '$card_uid'");
+                if ($result) {
+                    $response = array(
+                        'status' => 1,
+                        'message' => $nominal
+                    );
+                } else {
+                    $response = array(
+                        'status' => 0,
+                        'message' => 'Top up saldo gagal!'
+                    );
+                }
+                pakai_voucher($kode_voucher);
+            } else {
+                $response = array(
+                    'status' => -1,
+                    'message' => 'Voucher telah digunakan!'
+                );
+            }
+        } else {
+            $response = array(
+                'status' => -1,
+                'message' => 'Voucher tidak ditemukan!'
+            );
+        }
+    } else {
+        $response = array(
+            'status' => -1,
+            'message' => 'Silakan input kode voucher'
+        );
+    }
+    header('Content-Type: application/json');
+    echo json_encode($response);
+}
+
 ?>
